3f2e587104480f11d98edcaf832fc5c20b743d14
janbuca <janbuca333@gmail.com>
2024-02-13 00:55:05 -0800

    The Carousel will Done

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import CarouselEffect from './Componets/Header/Carousel/CarouselEffect';
 import Header from './Componets/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <CarouselEffect />
     </div>
   );
 }
--- amazon-clone/src/Componets/Header/Carousel/CarouselEffect.js
+++ amazon-clone/src/Componets/Header/Carousel/CarouselEffect.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import classes from './Carousel.module.css';
+import {Carousel} from "react-responsive-carousel";
+import {img} from './image/data';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function CarouselEffect() {
   return (
-    <div>CarouselEffect</div>
+    <div>
+        <Carousel 
+            autoPlay={true}
+            infiniteLoop={true}
+            showIndicators={false}
+            showThumbs={false}
+        >
+            {img.map((imageItemLink) => {
+          return <img src={imageItemLink} />;
+        })}
+        </Carousel>
+        <div className={classes.hero__img}></div>
+    </div>
   )
 }
 
