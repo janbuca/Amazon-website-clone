60dbfd946834d5bfcb4e901b03bfc3a32db5bcbf
janbuca <janbuca333@gmail.com>
2024-02-12 23:55:23 -0800

    The Header Part will be done

---
+++ amazon-clone/src/Componets/Header/Header.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import classes from "./Header.module.css";
+import { BsSearch } from "react-icons/bs";
+import { SlLocationPin } from "react-icons/sl";
+import { BiCart } from "react-icons/bi";
+
+function Header() {
+  return (
+    <section className={classes.fixed}>
+      <section>
+        <div className={classes.header__container}>
+          {/* logo section */}
+          <div className={classes.logo__container}>
+            <a href="/">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo"
+              />
+            </a>
+            <div className={classes.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliver to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
+
+
+          {/* search section */}
+          <div className={classes.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" />
+            <BsSearch size={38} />
+          </div>
+
+
+          {/* other section */}
+          <div className={classes.order__container}>
+            <a href=""  className={classes.language}>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </a>
+            <a href="">
+            <div>
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </div>
+          </a>
+          <a href="">
+            <p>returns</p>
+            <span>& Orders</span>
+          </a>
+          <a href="">
+            <span>0</span>
+          </a>
+          </div>
+
+        </div>
+
+        
+
+        <div>
+          
+        
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default Header;
