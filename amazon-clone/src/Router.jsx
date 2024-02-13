2589bf972f1018310f2631d0dd03977ea88a5f58
janbuca <janbuca333@gmail.com>
2024-02-13 03:25:29 -0800

    Router Routing folder will be Creating

---
+++ amazon-clone/src/Router.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    redirect,
+  } from "react-router-dom";
+
+function Router() {
+  return (
+    <div>Router</div>
+  )
+}
+
+export default Router
