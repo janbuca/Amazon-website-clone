533d80ae66ad173b1ac46e6a857304278230f54a
janbuca <janbuca333@gmail.com>
2024-02-13 02:48:05 -0800

    Single Product Componet will be done it also Product css

--- amazon-clone/src/Componets/Product/ProductCard.jsx
+++ amazon-clone/src/Componets/Product/ProductCard.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import Rating from '@mui/material/Rating';
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat';
+import classes from './Product.module.css'
 
-function ProductCard() {
+function ProductCard({product,flex,renderDesc,renderAdd}) {
+  const { image, title, id, rating, price,description  } = product;
   return (
-    <div>
+    <div className={`${classes.card__container}`}>
       <a href="">
-        <img src="" alt="" />
+        <img src={image} alt="" />
       </a>
       <div>
-        <h3>title</h3>
-        <div>
+        <h3>{title}</h3>
+        <div className={classes.rating}>
           {/* rating */}
-          <Rating value={5} precision={0.1}/>
-          {/*count  */}
-          <small>{50}</small>
+          <Rating value={rating?.rate} precision={0.1}/>
+                  {/*count  */}
+                  <small>{rating?.count}</small>
           {/* Price */}
         </div>
         <div>
           {/* price */}
-          <CurrencyFormat />
-
+          <CurrencyFormat amount={price} />
         </div>
+        <button className={classes.button}>
+          add to chat
+        </button>
       </div>
     </div>
   )
