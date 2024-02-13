9217a028f9cbe647ac1e5a6c420714f5e186aace
janbuca <janbuca333@gmail.com>
2024-02-13 03:35:42 -0800

    All Element in the componets can be adding in the Router

--- amazon-clone/src/Router.jsx
+++ amazon-clone/src/Router.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import Landing from './Pages/Landing/Landing';
+import Auth from './Pages/Auth/Auth';
+import Payment from './Pages/Payment/Payment';
+import Orders from './Pages/Orders/Orders';
+import Cart from './Pages/Cart/Cart';
 import {
     BrowserRouter as Router,
     Routes,
     Route,
-    redirect,
   } from "react-router-dom";
 
-function Router() {
+function Routing() {
   return (
-    <div>Router</div>
+    <Router>
+        <Routes>
+        <Route path="/" element={<Landing  />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/payments" element={<Payment />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/cart" element={<Cart />} />
+
+        </Routes>
+        
+    </Router>
   )
 }
 
-export default Router
+export default Routing
