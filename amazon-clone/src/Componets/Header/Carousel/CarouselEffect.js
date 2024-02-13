bb8f3813c3b914011c082e9d5cc7c2f95f58ba22
janbuca <janbuca333@gmail.com>
2024-02-13 00:40:44 -0800

    the CarouselEffect, Carousel Folder will be create

---
+++ amazon-clone/src/Componets/Header/Carousel/CarouselEffect.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Carousel.module.css';
+
+function CarouselEffect() {
+  return (
+    <div>CarouselEffect</div>
+  )
+}
+
+export default CarouselEffect
