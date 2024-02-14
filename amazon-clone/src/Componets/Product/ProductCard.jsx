ce1c11ea051525e78304170ea5031001de87fd51
janbuca <janbuca333@gmail.com>
2024-02-13 23:05:56 -0800

    Will be done addition of description on single product

--- amazon-clone/src/Componets/Product/ProductCard.jsx
+++ amazon-clone/src/Componets/Product/ProductCard.jsx
@@ -13,6 +13,7 @@ function ProductCard({product,flex,renderDesc,renderAdd}) {
       </Link>
       <div>
         <h3>{title}</h3>
+        {renderDesc && <div style={{maxWidth:"750px"}}>{description}</div>}
         <div className={classes.rating}>
           {/* rating */}
           <Rating value={rating?.rate} precision={0.1}/>
