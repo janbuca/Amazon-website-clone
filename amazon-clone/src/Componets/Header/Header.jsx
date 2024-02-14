c3ecbd15b15292187a72bdd59f2803b276b82325
janbuca <janbuca333@gmail.com>
2024-02-14 00:19:06 -0800

    Add to cart functionality can be done

--- amazon-clone/src/Componets/Header/Header.jsx
+++ amazon-clone/src/Componets/Header/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Header.module.css";
 import { BsSearch } from "react-icons/bs";
 import { SlLocationPin } from "react-icons/sl";
 import { BiCart } from "react-icons/bi";
 import LowerHeader from "./LowerHeader";
 import { Link } from "react-router-dom";
+import { DataContext } from "../DataProvider/DataProvider";
 
 function Header() {
+  const [{basket}, dispatch] = useContext(DataContext);
+  console.log(basket.length);
   return (
     <section className={classes.fixed}>
       <section>
@@ -62,7 +65,7 @@ function Header() {
             </Link>
             <Link to="/cart"className={classes.cart}>
               <BiCart size={35} />
-              <span>0</span>
+              <span>{basket.length}</span>
             </Link>
           </div>
         </div>
--- amazon-clone/src/Componets/Product/ProductCard.jsx
+++ amazon-clone/src/Componets/Product/ProductCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Rating from '@mui/material/Rating';
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat';
 import classes from './Product.module.css'
 import { Link } from 'react-router-dom';
+import { Type } from '../../Utility/action.type';
+import { DataContext } from '../DataProvider/DataProvider';
 
 function ProductCard({product,flex,renderDesc,renderAdd}) {
   const { image, title, id, rating, price,description  } = product;
+  const [state,dispatch]=useContext(DataContext)
+
+  const addToCart = ()=>{
+    dispatch({
+        type:Type.ADD_TO_BASKET,
+        item:{
+            image, title, id, rating, price,description
+        }
+    })
+  }
+
   return (
     <div className={`${classes.card__container}  ${flex?classes.product__flexed : ''}`}>
       <Link to={`/products/${id}`}>
@@ -25,7 +38,7 @@ function ProductCard({product,flex,renderDesc,renderAdd}) {
           {/* price */}
           <CurrencyFormat amount={price} />
         </div>
-        <button className={classes.button}>
+        <button className={classes.button} onClick={addToCart}>
           add to chat
         </button>
       </div>
