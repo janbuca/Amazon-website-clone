b18b4805160987124a0f88411868648eb40cfcd7
janbuca <janbuca333@gmail.com>
2024-02-14 00:51:17 -0800

    Subtotal and currencyFormat will be add

--- amazon-clone/src/Pages/Cart/Cart.jsx
+++ amazon-clone/src/Pages/Cart/Cart.jsx
@@ -2,10 +2,16 @@ import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import LayOut from "../../Componets/LayOut/LayOut";
 import { DataContext } from "../../Componets/DataProvider/DataProvider";
-import ProductCard from '../../Componets/Product/ProductCard.jsx'
+import ProductCard from "../../Componets/Product/ProductCard.jsx";
+import CurrencyFormat from "../../Componets/CurrencyFormat/CurrencyFormat.jsx";
+import { Link } from "react-router-dom";
 
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
+
+  const total = basket.reduce((amount,item)=>{
+    return item.price * item.amount + amount
+  },0)
 
   return (
     <LayOut>
@@ -32,7 +38,20 @@ function Cart() {
             })
           )}
         </div>
-        <div></div>
+
+        {basket?.length !== 0 && (
+          <div className={classes.subtotal}>
+            <div>
+              <p>Subtotal ({basket?.length} items)</p>
+              <CurrencyFormat amount={total} />
+            </div>
+            <span>
+              <input type="checkbox" />
+              <small>This order contains a gift</small>
+            </span>
+            <Link to="/payments">Continue to checkout</Link>
+          </div>
+        )}
       </section>
     </LayOut>
   );
